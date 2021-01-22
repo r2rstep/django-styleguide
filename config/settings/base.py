0860105ee0994f920cd9a4c2c4fafba293183377
r2r <aart.st@gmail.com>
2021-01-22 10:59:04 +0100

    fix running tests in docker

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,20 +93,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///django_template'),
-}
+DATABASES = {"default": {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ['POSTGRES_DB'],
+    'USER': os.environ['POSTGRES_USER'],
+    'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+    'HOST': os.environ['POSTGRES_HOST'],
+    'PORT': os.environ['POSTGRES_PORT'],
+}}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
--- docker/backend.dockerfile
+++ docker/backend.dockerfile
@@ -2,8 +2,15 @@ FROM python:3.8-slim-buster
 
 WORKDIR /app/
 
+RUN apt-get update \
+  # dependencies for building Python packages
+  && apt-get install -y build-essential \
+  # psycopg2 dependencies
+  && apt-get install -y libpq-dev
+
+ARG REQUIREMENTS_FILE_NAME=production.txt
 COPY requirements /app/requirements
-RUN pip install --no-cache-dir -r /app/requirements/local.txt
+RUN pip install --no-cache-dir -r /app/requirements/$REQUIREMENTS_FILE_NAME
 
 COPY docker/entrypoint /entrypoint
 RUN sed -i 's/\r$//g' /entrypoint
